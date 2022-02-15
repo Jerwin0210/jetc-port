6caad94765406106b0b1cf7205a6b134f42299a0
Jerwin0210 <52456477+Jerwin0210@users.noreply.github.com>
2022-02-15 17:36:47 +0800

    Add files via upload

---
+++ jetc-portofolio/js/script.js
@@ -0,0 +1,14 @@
+// ============TYPING ANIMATION ===============
+var typed = new Typed(".typing", {
+    strings: ["", "Web Developer", "Web Designer", "Computer Tech"],
+    typeSpeed: 100,
+    BackSpeed: 100,
+    loop: true
+})
+
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+menu_btn.addEventListener("click", () => {
+    menu_btn.classList.toggle("is-active");
+    mobile_menu.classList.toggle("is-active")
+})
