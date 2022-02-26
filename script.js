3c2b3f908f065e9b62abd0e9a428a3f132c1ee1e
Jerwin0210 <52456477+Jerwin0210@users.noreply.github.com>
2022-02-26 15:02:46 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // ============TYPING ANIMATION ===============
-var typed = new Typed(".typing", {
-    strings: ["", "Web Developer", "Web Designer", "Computer Tech"],
-    typeSpeed: 100,
-    BackSpeed: 100,
-    loop: true
-})
+const texts = ['Web Developer', 'Web Designer', 'Computer Technician']
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+
+    if (count === texts.length) {
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index)
+
+    document.querySelector('.typing').textContent = letter;
+
+    if (letter.length === currentText.length) {
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 200)
+
+}())
 
 const menu_btn = document.querySelector('.hamburger');
 const mobile_menu = document.querySelector('.mobile-nav');
